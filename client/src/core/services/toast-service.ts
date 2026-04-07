6172d617bbd2af3bfa43f247798e2770ac70cf7d
Jacek <jacekprazak1987@gmail.com>
2026-04-07 17:39:14 +0200

    Routing and app init enabled

---
+++ client/src/core/services/toast-service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+
+  constructor() {
+    this.createToastContainer();
+  }
+
+  private createToastContainer() {
+    if (!document.getElementById('toast-container')) {
+      const container = document.createElement('div')
+      container.id = 'toast-container'
+      container.className = 'toast toast-bottom toast-end'
+      document.body.appendChild(container)
+    }
+  }
+
+  private createToastElement(message: string, alertClass: string, duration = 5000) {
+    const toastContainer = document.getElementById('toast-container')
+    if (!toastContainer) return;
+
+    const toast = document.createElement('div')
+    toast.classList.add('alert', alertClass, 'shadow-lg')
+    toast.innerHTML = `
+      <span>${message}</span>
+      <button class="ml-4 btn btn-sm btn-ghost">x</button>
+    `
+
+    toast.querySelector('button')?.addEventListener('click', () => {
+      toastContainer.removeChild(toast)
+    })
+
+    toastContainer.append(toast)
+
+    setTimeout(() => {
+      if (toastContainer.contains(toast)) {
+        toastContainer.removeChild(toast)
+      }
+    }, duration)
+  }
+
+  success(messagge: string, duration?: number) {
+    this.createToastElement(messagge, 'alert-success', duration)
+  }
+
+  error(messagge: string, duration?: number) {
+    this.createToastElement(messagge, 'alert-error', duration)
+  }
+
+  warning(messagge: string, duration?: number) {
+    this.createToastElement(messagge, 'alert-warning', duration)
+  }
+
+  info(messagge: string, duration?: number) {
+    this.createToastElement(messagge, 'alert-info', duration)
+  }
+
+}
---
+++ client/src/features/members/memeber-list/memeber-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memeber-list',
+  imports: [],
+  templateUrl: './memeber-list.html',
+  styleUrl: './memeber-list.css',
+})
+export class MemeberList {}
